3ae1a14704f59f4f8868fb2a1b20d775586bc8b2
Prabhat Dutt <prabhatdutt95@gmail.com>
2021-03-08 00:55:29 +0530

    Added indentation

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pandas_datareader as web
 from sqlalchemy import create_engine
 
-# Twitter related Libraries
+# Twitter related Library
 import tweepy
 
 import os
@@ -22,6 +22,8 @@ stock_list = json.loads(df.to_json(orient="records"))
 stock_list = {'res': stock_list}
 
 # The various key are being taken from env file for Twitter authentication
+
+
 def authentication():
     consumer_key = os.environ.get('Twitter_Consumer_Key')
     consumer_secret = os.environ.get('Twitter_Consumer_Secret')
@@ -33,6 +35,8 @@ def authentication():
     return api
 
 # To handle any Page not Found Error
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return "<h1>404</h1><p>The resource could not be found.</p>", 404
@@ -45,6 +49,8 @@ def get_stock_list():
     return stock_list
 
 # This is used to predict the stock prices by taking the value from database for respective stock
+
+
 @app.route('/predict', methods=['GET'])
 def get_predictions():
     if 'id' in request.args:
@@ -55,24 +61,30 @@ def get_predictions():
         engine = create_engine('sqlite:///{}'.format(database_filename))
         df = pd.read_sql(table_name, engine)
 
-        return {'res' : [{k:df.values[i][v] for v,k in enumerate(df.columns)} for i in range(len(df)) ] }
+        return {'res': [{k: df.values[i][v] for v, k in enumerate(df.columns)} for i in range(len(df))]}
 
 # To get a list of tweets from twitter for a particular keywork and number of tweets
+
+
 def get_tweets(keyword, noOfTweet):
     api = authentication()
-    tweets = tweepy.Cursor(api.search, q=keyword, tweet_mode = "extended", languages=["en"]).items(noOfTweet)
-    tweets_list = [{'name':tweet.user.name, 'screen_name': tweet.user.screen_name, 'tweet' : tweet.full_text, 'polarity': TextBlob(tweet.full_text).sentiment.polarity } for tweet in tweets]
+    tweets = tweepy.Cursor(api.search, q=keyword, tweet_mode="extended", languages=[
+                           "en"]).items(noOfTweet)
+    tweets_list = [{'name': tweet.user.name, 'screen_name': tweet.user.screen_name, 'tweet': tweet.full_text,
+                    'polarity': TextBlob(tweet.full_text).sentiment.polarity} for tweet in tweets]
     return tweets_list
 
 # To generate twitter Sentiment analysis for a list of tweets
-# Polarity for each tweets is analysed and percentage is generated for each polarity i.e.,[Positive, Neutral, Negative] 
+# Polarity for each tweets is analysed and percentage is generated for each polarity i.e.,[Positive, Neutral, Negative]
+
+
 @app.route('/tweets', methods=['GET'])
-def get_polarity(): # http://127.0.0.1:5000/tweets?keyword=NSE
+def get_polarity():  # http://127.0.0.1:5000/tweets?keyword=NSE
     tweeter_result = {}
     if 'keyword' in request.args:
         keyword = request.args['keyword']
         noOfTweet = 150
-        polarity_data = [0]*3 # [Positive, Neutral, Negative]
+        polarity_data = [0]*3  # [Positive, Neutral, Negative]
         tweets_list = get_tweets(keyword, noOfTweet)
         for data in tweets_list:
             if data['polarity'] > 0:
@@ -89,19 +101,24 @@ def get_polarity(): # http://127.0.0.1:5000/tweets?keyword=NSE
     return tweeter_result
 
 # To provide past data of a particular stock and 'n' number of days ago
+
+
 @app.route('/pastData', methods=['GET'])
-def get_previous_days_data(): # http://127.0.0.1:5000/pastData?code=AXISBANK&days=15
+def get_previous_days_data():  # http://127.0.0.1:5000/pastData?code=AXISBANK&days=15
     previous_days_data = {}
     if 'code' in request.args and 'days' in request.args:
         code = request.args['code']
         no_of_days = int(request.args['days'])
         start_date = datetime.today() - timedelta(no_of_days)
         end_date = datetime.today()
-        previous_days_data = web.DataReader(code+'.NS', data_source='yahoo', start=datetime.strftime(start_date, '%Y-%m-%d'), end=datetime.strftime(end_date, '%Y-%m-%d')).round(2)
+        previous_days_data = web.DataReader(code+'.NS', data_source='yahoo', start=datetime.strftime(
+            start_date, '%Y-%m-%d'), end=datetime.strftime(end_date, '%Y-%m-%d')).round(2)
         previous_days_data.reset_index(level=0, inplace=True)
-        previous_days_data = json.loads(previous_days_data.to_json(orient="records"))
+        previous_days_data = json.loads(
+            previous_days_data.to_json(orient="records"))
         previous_days_data = {'res': previous_days_data}
     return previous_days_data
+
 
 if __name__ == '__main__':
     app.run(debug=True)
