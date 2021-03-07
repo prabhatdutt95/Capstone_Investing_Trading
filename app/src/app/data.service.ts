6546a009155c32ccf6e406937ef297284943eb47
Prabhat Dutt <prabhatdutt95@gmail.com>
2021-03-08 04:35:56 +0530

    Added screenshots

--- app/src/app/data.service.ts
+++ app/src/app/data.service.ts
@@ -6,21 +6,21 @@ import { Observable } from 'rxjs';
 })
 export class DataService {
 
-  allStocksUrl = "https://stockdatatryout.herokuapp.com/all";
+  stockUrl = "http://127.0.0.1:5000/";
   constructor(private httpClient: HttpClient) { }
 
   getAllStocks(): Observable<any>{
-    return this.httpClient.get<any>(this.allStocksUrl);
+    return this.httpClient.get<any>(this.stockUrl + 'all');
   }
   getPediction(keyword): Observable<any> {
-    return this.httpClient.get<any>('https://stockdatatryout.herokuapp.com/tweets', {
+    return this.httpClient.get<any>(this.stockUrl + 'tweets', {
       params: {
         keyword
       },
     })
   }
   getPastData(code, days): Observable<any> {
-    return this.httpClient.get<any>('https://stockdatatryout.herokuapp.com/pastData', {
+    return this.httpClient.get<any>(this.stockUrl + 'pastData', {
       params: {
         code: code,
         days: days
@@ -29,7 +29,7 @@ export class DataService {
   }
 
   getPredictedData(code): Observable<any> {
-    return this.httpClient.get<any>('https://stockdatatryout.herokuapp.com/predict', {
+    return this.httpClient.get<any>(this.stockUrl + 'predict', {
       params: {
         id: code,
       },
